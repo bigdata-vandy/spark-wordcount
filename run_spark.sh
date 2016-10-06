f14343eb0044240fea50bcacb715e0264e3c6272
arnold-jr <j.arnold.111@gmail.com>
2016-10-06 18:15:15 -0500

    Updating run_spark.sh

--- run_spark.sh
+++ run_spark.sh
@@ -8,39 +8,36 @@ fi
 #input1=spark_read_me.txt
 input1=stack/productivity/Posts.xml
 
-PROGRAM="
+APP="
     target/scala-2.11/spark-wc_2.11-1.0.jar \
     $input1
     "
 
-export SPARK_HOME=/usr/lib/spark/
-
-
-flag=0
+flag=2
 if [ ${flag} == 0 ]
 then
   # Run application locally
   $SPARK_HOME/bin/spark-submit \
     --class WordCountApp \
     --master local[*] \
-    $PROGRAM
+    $APP
 elif [ ${flag} == 1 ]
 then
-# Run on a Spark standalone cluster in client deploy mode
-./bin/spark-submit \
-  --class org.apache.spark.examples.SparkPi \
-  --master spark://207.184.161.138:7077 \
-  --executor-memory 20G \
-  --total-executor-cores 100 \
-    $PROGRAM
+  # Run on a Spark standalone cluster in client deploy mode
+  $SPARK_HOME/bin/spark-submit \
+    --class WordCountApp \
+    --master spark://207.184.161.138:7077 \
+    --executor-memory 50m \
+    --total-executor-cores 100 \
+    $APP
 elif [ ${flag} == 2 ]
 then
   # Run on a YARN cluster
   $SPARK_HOME/bin/spark-submit \
     --class WordCountApp \
     --master yarn \
-    --deploy-mode client \
+    --deploy-mode cluster \
     --executor-memory 50m \
     --num-executors 6 \
-    $PROGRAM
+    $APP
 fi
