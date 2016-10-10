4792f97071c5969d9f1d4d1451cd85c1f43b6bd3
arnold-jr <j.arnold.111@gmail.com>
2016-10-10 16:32:52 -0500

    Pointing to differernt hdfs input files.

--- run_spark.sh
+++ run_spark.sh
@@ -8,37 +8,38 @@ fi
 echo $SPARK_HOME
 
 #input1=spark_read_me.txt
-input1=stack/productivity/Posts.xml
+#input1=stack/math.stackexchange.com/Posts.xml.gz
+input1=stack/stackoverflow.stackexchange.com/Comments.xml.gz
+output_dir="hdfs:///user/arnoldjr/wordcount$(date +%Y-%m-%d_%H-%M-%S)"
+
+echo $output_dir
 
 APP="
     target/scala-2.11/spark-wc_2.11-1.0.jar \
-    $input1
+    $input1 \
+    $output_dir
     "
 
+
 flag=2
-if [ ${flag} == 0 ]
-then
+if [ ${flag} == 0 ]; then
   # Run application locally
   $SPARK_HOME/bin/spark-submit \
     --class WordCountApp \
     --master local[*] \
     $APP
-elif [ ${flag} == 1 ]
-then
+elif [ ${flag} == 1 ]; then
   # Run on a Spark standalone cluster in client deploy mode
   $SPARK_HOME/bin/spark-submit \
     --class WordCountApp \
     --master spark://vmp741.vampire:7077 \
     $APP
-elif [ ${flag} == 2 ]
-then
+elif [ ${flag} == 2 ]; then
   # Run on a YARN cluster
   $SPARK_HOME/bin/spark-submit \
     --class WordCountApp \
     --master yarn \
     --deploy-mode cluster \
-    --executor-memory 50m \
-    --driver-cores 1 \
-    --num-executors 2 \
+    --executor-memory 1G \
     $APP
 fi
--- src/main/scala-2.11/WordCountApp.scala
+++ src/main/scala-2.11/WordCountApp.scala
@@ -1,5 +1,5 @@
 /** WordCountApp.scala
-  * Created by joshuaarnold on 9/9/16.
+  * Created by arnold-jr on 9/9/16.
   */
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
@@ -8,11 +8,13 @@ import org.apache.spark.SparkConf
 object WordCountApp {
 
   def main(args: Array[String]) {
-    if (args.length != 1) {
+    if (args.length != 2) {
      System.err.println("Requires HDFS input(s) as argument")
      System.exit(1)
     }
     val textFileName: String = args(0)
+    val outputDir: String = args(1)
+
     val conf = new SparkConf()
       .setAppName("Word Count Application")
     val sc = new SparkContext(conf)
@@ -21,9 +23,13 @@ object WordCountApp {
     val wordFrequencies = textData flatMap (_ split ("\\s+")
       map (word => (word, 1))) reduceByKey (_ + _)
 
+
+    wordFrequencies.saveAsTextFile(outputDir)
+
     // Prints results
-    (wordFrequencies.takeOrdered(50)(Ordering[Int].reverse.on(_._2)))
+    wordFrequencies.takeOrdered(50)(Ordering[Int].reverse.on(_._2))
         .foreach(println)
 
+    println("hello")
   }
 }
